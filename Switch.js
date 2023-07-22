47a475a18279b86cfa615798b1554ba7ca7ba8ad
Bappy71 <19103076@iubat.edu>
2023-07-22 23:27:36 +0600

    first commit

--- Switch.js
+++ Switch.js
@@ -1,4 +1,25 @@
-var age = 10;
-if (age<20){
-    document.write("Shala Kamla")
+var Marks = 90;
+
+switch(true){
+    case (Marks<=100 && Marks>=80):
+    document.write('A+');
+    break;
+    case (Marks<=80 && Marks>=70):
+    document.write('A');
+    break;
+    case (Marks<=70 && Marks>=60):
+    document.write('A-');
+    break;
+    case (Marks<=60 && Marks>=50):
+    document.write('B');
+    break;
+    case (Marks<=50 && Marks>=40):
+    document.write('c');
+    break;
+    case (Marks<=40 && Marks>=33):
+    document.write('D');
+    break;
+
+    default:
+        document.write("Fail")
 }
