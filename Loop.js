eb1bb31af87fbf6d72b1b18af4f851972a795b17
Bappy71 <19103076@iubat.edu>
2023-07-24 04:13:34 +0600

    first commit

--- Loop.js
+++ Loop.js
@@ -1,13 +1,16 @@
-// var i;
 
-// for (i = 0; i < 10; i++) {
-//   if (i === 5 || i === 8) {
-//     continue;
-//   }
-//   document.write(i + "<button>Submit</button> <br>");
-// }
+//For Loop Break Statement In Loop
 
-//Break Statement In Loop
+var i;
+
+for (i = 0; i < 10; i++) {
+  if (i === 5 || i === 8) {
+    continue;
+  }
+  document.write(i + "<button>Submit</button> <br>");
+}
+
+//For Loop Break Statement In Loop
 
 var i;
 
@@ -16,4 +19,12 @@ for (i = 0; i < 10; i++) {
     break;
   }
   document.write(i + "<button>Submit</button> <br>");
+}
+
+//While Loop//
+
+var i = 0;
+while(i<10){
+  document.write(i + "<button>Submit</button> <br>");
+  i++
 }
